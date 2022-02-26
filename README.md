33b407765f5e1df24408a832bd230b5e8ea45270
Mostafa Aboelnaga <66335224+mostafa-aboelnaga@users.noreply.github.com>
2022-02-26 08:05:21 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,127 @@
 # Uniquely Building Parse Trees
  Building a parse tree from a non-parenthesized mathematical expression
+
+
+# ![Uniquely Building Parse Trees](https://github.com/mostafa-aboelnaga/Uniquely-Building-Parse-Trees/blob/main/DemoImages/AlgorithmIntroDemo.png?raw=true)
+
+## **How it works** 🤔
+
+First, [download](https://www.python.org/downloads/) and install **Python**. Version `3.7` or higher is recommended.
+
+
+Let's now run the main file, `uniqueParseTrees.py`, no requirements needed, etc.
+
+```bash
+python uniqueParseTrees.py
+```
+
+That's all you need to know to start!
+
+---
+
+You are now asked to input some non-parenthesized expression as the following
+
+```bash
+5 + 2 * 3 + 10 / 2
+```
+
+when given such input, the algorithm (implemented in the `buildingUniqueParseTrees` in the `uniqueParseTrees.py` file) is all about splitting the expression into properly ordered sub-expressions.
+
+Then, it starts building its own tree, using the first sub-expression, and so on, looping through the remaining sub-expressions.
+
+### Notice that
+
+The precedence of operators needs to be defined **manually** of course as follows:
+```python
+operators = {"-": 0, "+": 0, "*": 1, "/": 1}
+```
+
+<br>
+
+Here's a quick visual example of what is really happening:
+
+<br>
+
+## starting off by taking the first sub-expression, implementing it manually
+<img style="background: white" src="DemoImages/1.png" />
+
+## given an operator, we create a temporary tree with the operator as its root value, inserting a right child pointed at by `current2`
+
+<img style="background: white" src="DemoImages/2.png" />
+
+## now lets check the precedence
+`The operator * has higher precedence than +`
+
+<img style="background: white" src="DemoImages/3.0.png" />
+
+## In such case, we do the following steps:
+
+<img style="background: white" src="DemoImages/3.1.png" />
+<img style="background: white" src="DemoImages/3.2.png" />
+<img style="background: white" src="DemoImages/3.3.png" />
+
+## we are now left off at this **state**: 
+
+<img style="background: white" src="DemoImages/4.png" />
+
+## given an operand, we will store it in the position pointed at by `current2`
+
+<img style="background: white" src="DemoImages/4.1.png" />
+
+## here's the current **state** of our tree:
+<img style="background: white" src="DemoImages/4.2.png" />
+
+## given another operator, we create a temporary tree with the operator as its root value, inserting a right child pointed at by `current2`, once again.
+
+<img style="background: white" src="DemoImages/5.png" />
+
+## well, this time the operator stored at `t` or the `+` operator, has lower precedence. In such case, we do the following steps: 
+
+<img style="background: white" src="DemoImages/6.png" />
+
+
+<img style="background: white" src="DemoImages/6.1.png" />
+<img style="background: white" src="DemoImages/6.2.png" />
+<img style="background: white" src="DemoImages/6.3.png" />
+<img style="background: white" src="DemoImages/6.4.png" />
+<img style="background: white" src="DemoImages/6.5.png" />
+
+## we are now left off at this **state**, once again
+
+<img style="background: white" src="DemoImages/7.png" />
+
+## now, given an operand, store it in the position pointed at by `current2`
+
+<img style="background: white" src="DemoImages/7.1.png" />
+
+<br>
+<br>
+
+<h2 align="center" style="font-weight: bold;">and so on...</h2>
+
+<br>
+<br>
+
+## here's the final state of our uniquely built parse tree:
+
+<img style="background: white" src="DemoImages/8.png" />
+
+## and that's about it.
+
+<br>
+<br>
+
+## ⚒️ **Built with**
+
+<br>
+
+
+- [Python](https://www.w3schools.com/python/python_reference.asp) - Python is a popular programming language used in many fields.
+
+
+<br>
+
+
+## [License](https://github.com/mostafa-aboelnaga/Terminalio/blob/main/LICENSE)
+
+MIT © [Mostafa Aboelnaga](https://github.com/mostafa-aboelnaga/)
---
+++ uniqueParseTree.py
@@ -0,0 +1,191 @@
+import operator  # Required for 'evaluation' fn
+
+class newNode:
+    # Construct to create a new node
+    def __init__(self, key):
+        self.data = key
+        self.left = None
+        self.right = None
+
+
+# Implementation of data structures that were used throughout the algorithm:
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
+
+
+class BinaryTree:
+    def __init__(self, rootObj):
+        self.key = rootObj
+        self.right = None
+        self.left = None
+
+    def insertLeft(self, newNode):
+        if self.left == None:
+            self.left = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.left = self.left
+            self.left = t
+
+    def insertRight(self, newNode):
+        if self.right == None:
+            self.right = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.right = self.right
+            self.right = t
+
+    def getRootVal(self):
+        return self.key
+
+    def setRootVal(self, val):
+        self.key = val
+
+    def getRightChild(self):
+        return self.right
+
+    def getLeftChild(self):
+        return self.left
+
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+    def printQueue(self):
+        print(self.items)
+
+
+def breadthInTree(parseTree):
+    if parseTree is not None:
+        print(parseTree.getRootVal())
+    current = parseTree
+    myQ = Queue()
+    while current is not None:
+        if current.getLeftChild() is not None:
+            print(current.getLeftChild().getRootVal())
+            myQ.enqueue(current.getLeftChild())
+        if current.getRightChild() is not None:
+            print(current.getRightChild().getRootVal())
+            myQ.enqueue(current.getRightChild())
+        if myQ.isEmpty():
+            break
+        else:
+            current = myQ.dequeue()
+
+
+def bfs (tree):
+    root = tree
+    a = []
+    b = []
+    a.append(root)
+    even = True
+    while len(a) > 0:
+        current = a.pop(0)
+        if current.left != None:
+            b.append(current.left)
+        if current.right != None:
+            b.append(current.right)
+        if len(a) == 0:
+            if even == True:
+                even = False
+            else:
+                even = True
+            a = b
+            if even == False:
+                for i in range(len(b)):
+                    if i%2 != 0:
+                        print(b[i].data,end = " ")
+            b = []
+
+
+
+# Main algorithm, used to solve a non-parenthesized mathematical expression:
+def buildUniqueParseTree(expression):
+    # Splitting the given expression
+    exp = expression.split()
+
+    # Building our main tree using the first sub-expression
+    eTree = BinaryTree(exp[1])
+    eTree.insertLeft(int(exp[0]))
+    eTree.insertRight(int(exp[2]))
+
+    # Defining the precedence of operators
+    operators = {"-": 0, "+": 0, "*": 1, "/": 1}
+
+    # Looping through the remaining sub expression
+    for i in exp[3:]:
+        if i in "+-/*":
+            # Building a temporary tree
+            t = BinaryTree(i)
+            t.insertRight('')
+
+            # Traversing into its right child
+            current2 = t
+            current2 = current2.getRightChild()
+
+            # Checking the precedence
+            if operators[t.getRootVal()] <= operators[eTree.getRootVal()]:
+                t.left = eTree
+                eTree = t
+            else:
+                t.left = eTree.right
+                eTree.right = t
+
+        else:  # Token is an operand then
+            current2.setRootVal(int(i))  # Storing the given token (operand) into the right child
+    return eTree
+
+
+# Implementing some testing functions
+def evaluate(parseTree):
+    opers = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.truediv}
+    leftC = parseTree.getLeftChild()
+    rightC = parseTree.getRightChild()
+
+    if leftC and rightC:
+        fn = opers[parseTree.getRootVal()]
+        return fn(evaluate(leftC), evaluate(rightC))
+    else:
+        return parseTree.getRootVal()
+
+
+def inOrder(parseTree):
+    if parseTree is not None:
+        inOrder(parseTree.getLeftChild())
+        print(parseTree.getRootVal())
+        inOrder(parseTree.getRightChild())
+
+
+# Testing the whole thing
+myexp = input()
+myTree = buildUniqueParseTree(myexp)
+breadthInTree(myTree)
